e5a915be19b674d950dfaf7396fd67e2777f2462
k1berteftel <kkulis985@gmail.com>
2025-12-12 19:19:13 +0300

    cost show fix

--- utils/start_utils.py
+++ utils/start_utils.py
@@ -10,7 +10,7 @@ from database.action_data_class import DataInteraction
 async def start_schedulers(bot: Bot, session: DataInteraction, scheduler: AsyncIOScheduler):
     for user in await session.get_users():
         if user.sub:
-            await polling_user_sub(user.user_id, bot, session, scheduler)
+            #await polling_user_sub(user.user_id, bot, session, scheduler)
             job_id = f'polling_sub_{user.user_id}'
             scheduler.add_job(
                 polling_user_sub,
